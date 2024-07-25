7bdc1092f3f553162d7cf3bac90f7260adfb2b22
Guto <gutomourao05@gmail.com>
2024-07-24 23:40:28 -0300

    learning C# - mapper, context, dtos

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,39 +1,78 @@
 ﻿using System;
+using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
 using FilmesApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FilmesApi.Controllers
+namespace FilmesApi.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
 {
-	[ApiController]
-	[Route("[controller]")]
-	public class FilmeController: ControllerBase
-	{
-		private static List<Filme> filmes = new List<Filme>();
-		private static int id = 0;
+    private FilmeContext _context;
+    private IMapper _mapper;
 
-		[HttpPost]
-		public IActionResult AdicionaFilme([FromBody] Filme filme)
-		{
-			filme.Id = id++;
-			filmes.Add(filme);
-			return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
-		}
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
 
-		[HttpGet]
-		public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 5)
-		{
-			return filmes.Skip(skip).Take(take);
-		}
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
+    {
+        Filme filme = _mapper.Map<Filme>(filmeDto);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+    }
 
-		[HttpGet("{id}")]
-		public IActionResult RecuperaFilmePorId(int id)
-		{
-			var result = filmes.FirstOrDefault(filme => filme.Id == id);
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 5)
+    {
+        return _context.Filmes.Skip(skip).Take(take);
+    }
+     
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var result = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
-			if (result == null) return NotFound();
+        if (result == null) return NotFound();
 
-			return Ok(result);
-		}
-	}
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+
+        var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+        patch.ApplyTo(filmeParaAtualizar, ModelState);
+
+        if(!TryValidateModel(filmeParaAtualizar))
+        {
+            return ValidationProblem(ModelState);
+        }
+
+        _mapper.Map(filmeParaAtualizar, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
 }
 
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -5,17 +5,17 @@ namespace FilmesApi.Models
 {
 	public class Filme
 	{
-		[Key]
-		[Required]
-		public int Id { get; set; }
-		[Required(ErrorMessage = "O título é obrigatório")]
-		public string Titulo { get; set; }
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O título é obrigatório")]
+        public string Titulo { get; set; }
         [Required(ErrorMessage = "O Genero é obrigatório")]
-		[MaxLength(50, ErrorMessage = "O tamanho do genero não pode exceder 50")]
+        [StringLength(50, ErrorMessage = "O tamanho do genero não pode exceder 50")]
         public string Genero { get; set; }
         [Required(ErrorMessage = "A Duração é obrigatória")]
-		[Range(70,600, ErrorMessage = "Duracao deve t er entre 70 e 600 minutos")]
+        [Range(70, 600, ErrorMessage = "Duracao deve t er entre 70 e 600 minutos")]
         public int Duracao { get; set; }
-	}
+    }
 }
 
